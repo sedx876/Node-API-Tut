eba1f612064bbe7578cb97a59da46149b8a24b24
sedx876 <swatkins876@gmail.com>
2020-10-27 06:28:22 -0400

    added server.js

---
+++ server.js
@@ -0,0 +1,8 @@
+const http = require('http')
+
+const server = http.createServer((req, res) => {})
+
+const PORT = process.env.PORT || 5000
+
+server.listen(PORT, () => 
+console.log(`Server running on Port ${PORT}`))
